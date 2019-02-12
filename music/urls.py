2b314906773cc5ea467dddd7fb8d5ff3e439e22e
will <atkinsonwill3@gmail.com>
2019-02-12 15:56:58 +1000

    fixed views and added more to details page

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "music"
+
 urlpatterns = [
     # /music/ - Homepage
     url(r'^$', views.index, name='index'),
 
-    # /music/712/ - Album Details
+    # /music/<album_id>/ - Album Details
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
 
 def index(request):
@@ -7,8 +6,5 @@ def index(request):
     return render(request, 'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id);
     return render(request, 'music/detail.html', {'album' : album})
