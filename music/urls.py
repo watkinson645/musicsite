74161362b3138a24c8cb1da3e04928746fa3cd08
will <atkinsonwill3@gmail.com>
2019-02-12 15:33:19 +1000

    set up index and detail pages

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /music/ - Homepage
     url(r'^$', views.index, name='index'),
+
+    # /music/712/ - Album Details
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,14 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album, Song
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage<h1>");
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums' : all_albums})
+
+def detail(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album' : album})
