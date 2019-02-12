c4428eb42473ebb20aa41698d46b8f85c5592de6
will <atkinsonwill3@gmail.com>
2019-02-12 14:52:50 +1000

    set up database

--- music/models.py
+++ music/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    genre = models.CharField(max_length=256)
+    logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title + " - " + self.artist
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    ext = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage");
+    return HttpResponse("<h1>This is the music app homepage<h1>");
