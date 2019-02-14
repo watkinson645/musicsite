dd6f45cba0399f05f4f59c79138c41c022df74a9
will <atkinsonwill3@gmail.com>
2019-02-14 18:13:18 +1000

    added homepage

--- musicSite/urls.py
+++ musicSite/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.home),
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
 ]
